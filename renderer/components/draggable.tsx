aaabd2a6771f2d55fbd9c7faa8e758e4aa4d6800
David <david@davidcrandall.com>
2023-04-08 16:34:31 -0700

    styling

--- renderer/components/draggable.tsx
+++ renderer/components/draggable.tsx
@@ -5,7 +5,7 @@ export default function DraggableBox(props) {
     const [expanded, toggleExpanded] = useState(true)
     return (
         <Draggable bounds="parent">
-        <div  className="bg-black/[0.6] absolute z-10 right-[50px] cursor-move"
+        <div  className="bg-black/[0.5] absolute z-10 right-[50px] cursor-move"
             style={{
                 top: props.y ? props.y : '100px',
                 left: props.align == 'left' && props.x ? props.x : 'initial',
@@ -15,7 +15,7 @@ export default function DraggableBox(props) {
             }}
         
         >
-            <div className="bg-black px-4 py-2 w-full">
+            <div className="bg-black/[0.7] px-4 py-2 w-full">
                 <p className="inline text-base uppercase">Control panel</p>
                 <button className="inline float-right text-xl line-height-0 mt-[-4px]" onClick={() =>{toggleExpanded(!expanded)}}>
                     {expanded ? '-' : '+'}
